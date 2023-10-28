3c1b67b2aa1873ee9ee4b8e85ad7a92412679548
Dmitriy Dubson <d.dubson@gmail.com>
2023-10-28 11:57:39 -0400

    WIP

---
+++ apps/jellyfin-web/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  "jellyfin": {
+    //"web": "https://demo.jellyfin.org/unstable/web"
+    "web": "http://localhost:8080"
+  }
+}
+
+export default config;
--- apps/jellyfin-web/pageobjects/page.ts
+++ apps/jellyfin-web/pageobjects/page.ts
@@ -2,12 +2,14 @@
 * main page object containing all methods, selectors and functionality
 * that is shared across all page objects
 */
+import config from "../config";
+
 export default class Page {
     /**
     * Opens a sub page of the page
     * @param path path of the sub page (e.g. /path/to/page.html)
     */
     public open (path: string) {
-        return browser.url(`https://demo.jellyfin.org/unstable/web/#${path}`)
+        return browser.url(`${config.jellyfin.web}/#${path}`)
     }
 }
